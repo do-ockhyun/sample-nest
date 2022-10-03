b32178be563ed4f6954bf0eeb8b5811006446cfd
옥동자 <do.ockhyun@gmail.com>
2022-10-03 22:51:44 +0900

    db

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@InjectEntityManager() private entityManager: EntityManager,) {
+
+  }
+  
+  async getHello(): Promise<string> {
+    const [now] = await this.entityManager.query('select now()')    
+    return 'Hello World - ' + now['now()'];
   }
 }
